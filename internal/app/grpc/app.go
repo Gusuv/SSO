50442fad4b9668f6ed93572041b6751ab3d4ab65
Gusuv <papikanartur3@gmail.com>
2026-02-10 23:58:02 +0300

    feat: App srtuct

---
+++ internal/app/grpc/app.go
@@ -0,0 +1,26 @@
+package app
+
+import (
+	"log/slog"
+	authRPC "main/internal/grpc/auth"
+
+	"google.golang.org/grpc"
+)
+
+type App struct {
+	log        *slog.Logger
+	gRPCServer *grpc.Server
+	port       int
+}
+
+func New(log *slog.Logger, port int) *App {
+	gRPCServer := grpc.NewServer()
+
+	authRPC.Register(gRPCServer)
+
+	return &App{
+		log:        log,
+		gRPCServer: gRPCServer,
+		port:       port,
+	}
+}
