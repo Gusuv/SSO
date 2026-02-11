588618544a4d09db28e6700cc1e3635b011ecb44
Gusuv <papikanartur3@gmail.com>
2026-02-12 00:05:35 +0300

    feat: Add Run method

--- internal/app/grpc/app.go
+++ internal/app/grpc/app.go
@@ -1,8 +1,10 @@
 package appgrpc
 
 import (
+	"fmt"
 	"log/slog"
 	authRPC "main/internal/grpc/auth"
+	"net"
 
 	"google.golang.org/grpc"
 )
@@ -23,4 +25,24 @@ func New(log *slog.Logger, port int) *App {
 		gRPCServer: gRPCServer,
 		port:       port,
 	}
+}
+
+func (a *App) Run() error {
+	const oper = "appgrpc.Run"
+
+	log := a.log.With(slog.String("oper", oper), slog.Int("port", a.port))
+
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", a.port))
+
+	if err != nil {
+		return fmt.Errorf("%s: %w:", oper, err)
+	}
+
+	log.Info("gRPC Run", slog.String("addr", l.Addr().String()))
+
+	if err := a.gRPCServer.Serve(l); err != nil {
+		return fmt.Errorf("%s: %w:", oper, err)
+	}
+
+	return nil
 }
