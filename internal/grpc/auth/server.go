0e82daedb690fda755ba0f5a711b3411f4d6c5fb
Gusuv <papikanartur3@gmail.com>
2026-02-20 23:57:07 +0300

    fix: Renaming

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -7,26 +7,29 @@ import (
 	"google.golang.org/grpc"
 )
 
+type Auth interface {
+}
+
 type serverAPI struct {
-	sso1.UnimplementedAuthServer
+	sso1.UnimplementedAuthServiceServer
 }
 
 func Register(grpc *grpc.Server) {
-	sso1.RegisterAuthServer(grpc, &serverAPI{})
+	sso1.RegisterAuthServiceServer(grpc, &serverAPI{})
 }
 
-func (s *serverAPI) Login(ctx context.Context, req *sso1.LoginReq) (*sso1.LoginResp, error) {
+func (s *serverAPI) Login(ctx context.Context, eq *sso1.LoginRequest) (*sso1.LoginResponse, error) {
 	panic("In development")
 }
 
-func (s *serverAPI) Register(ctx context.Context, req *sso1.RegisterReq) (*sso1.RegisterResp, error) {
+func (s *serverAPI) Register(ctx context.Context, req *sso1.RegisterRequest) (*sso1.RegisterResponse, error) {
 	panic("In development")
 }
 
-func (s *serverAPI) Logout(ctx context.Context, req *sso1.LogoutReq) (*sso1.LogoutResp, error) {
+func (s *serverAPI) Logout(ctx context.Context, req *sso1.LogoutRequest) (*sso1.LogoutResponse, error) {
 	panic("In development")
 }
 
-func (s *serverAPI) AdminCheck(ctx context.Context, req *sso1.AdminCheckReq) (*sso1.AdminCheckResp, error) {
+func (s *serverAPI) AdminCheck(ctx context.Context, req *sso1.AdminCheckRequest) (*sso1.AdminCheckResponse, error) {
 	panic("In development")
 }
