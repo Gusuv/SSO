6d6004c9749a0731a7cd7123dc78bfcc8ec80bfa
Gusuv <papikanartur3@gmail.com>
2026-02-21 23:29:51 +0300

    feat: Login validation

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -2,9 +2,12 @@ package auth
 
 import (
 	"context"
+	"strings"
 
 	sso1 "github.com/Gusuv/sso-protos/generated/go/sso"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
 )
 
 type Auth interface {
@@ -18,8 +21,17 @@ func Register(grpc *grpc.Server) {
 	sso1.RegisterAuthServiceServer(grpc, &serverAPI{})
 }
 
-func (s *serverAPI) Login(ctx context.Context, eq *sso1.LoginRequest) (*sso1.LoginResponse, error) {
-	panic("In development")
+func (s *serverAPI) Login(ctx context.Context, req *sso1.LoginRequest) (*sso1.LoginResponse, error) {
+	email := strings.TrimSpace(req.GetEmail())
+	password := strings.TrimSpace(req.GetPassword())
+
+	if email == "" {
+		return nil, status.Error(codes.InvalidArgument, "Email is required")
+	}
+	if password == "" {
+		return nil, status.Error(codes.InvalidArgument, "Password is required")
+	}
+	return &sso1.LoginResponse{}, nil
 }
 
 func (s *serverAPI) Register(ctx context.Context, req *sso1.RegisterRequest) (*sso1.RegisterResponse, error) {
