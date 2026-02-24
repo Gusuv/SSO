7c369719e1f094f4f272ebad5c6cd24a49346e9a
Gusuv <papikanartur3@gmail.com>
2026-02-24 23:59:18 +0300

    feat: Required

--- internal/validation/register.go
+++ internal/validation/register.go
@@ -1,9 +1,28 @@
 package validation
 
+import "errors"
+
 func RegisterValidation(username, email, password string) error {
+	if err := requiredRegster(username, email, password); err != nil {
+		return err
+	}
 	if err := passwordLen(password); err != nil {
 		return err
 	}
 	// In development
+	return nil
+}
+
+func requiredRegster(username, email, password string) error {
+
+	if username == "" {
+		return errors.New("Username is require")
+	}
+	if email == "" {
+		return errors.New("Email is required")
+	}
+	if password == "" {
+		return errors.New("Password is required")
+	}
 	return nil
 }
