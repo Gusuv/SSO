22bbb81dcabdfa527ba5b863b997272e31d5e07f
Gusuv <papikanartur3@gmail.com>
2026-02-25 23:52:54 +0300

    fix: AppID validation

--- internal/validation/login.go
+++ internal/validation/login.go
@@ -5,8 +5,8 @@ import (
 	"strings"
 )
 
-func LoginValidation(email, password string) error {
-	if err := notEmpty(email, password); err != nil {
+func LoginValidation(email, password string, appId int64) error {
+	if err := notEmpty(email, password, appId); err != nil {
 		return err
 	}
 	if err := passwordLen(password); err != nil {
@@ -19,7 +19,7 @@ func LoginValidation(email, password string) error {
 	return nil
 }
 
-func notEmpty(email, password string) error {
+func notEmpty(email, password string, appId int64) error {
 
 	if email == "" {
 		return errors.New("Email is required")
@@ -27,12 +27,15 @@ func notEmpty(email, password string) error {
 	if password == "" {
 		return errors.New("Password is required")
 	}
+	if appId == 0 {
+		return errors.New("AppID required")
+	}
 	return nil
 }
 
 func passwordLen(password string) error {
 	if len(password) < 8 {
-		return errors.New("Password is short")
+		return errors.New("Password too short")
 	}
 	return nil
 }
