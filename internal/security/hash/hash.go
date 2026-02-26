40a1956bd173fd9600af7838b7d4689f901af4c6
Gusuv <papikanartur3@gmail.com>
2026-02-26 20:31:10 +0300

    fix: Name correction

--- internal/security/hash/hash.go
+++ internal/security/hash/hash.go
@@ -6,10 +6,11 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-func PaasswordHash(password string) (error, string) {
+func PasswordHash(password string) (error, string) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
-	if err != nil {
-		return errors.New("Password is npot hashed"), ""
-	}
 
+	if err != nil {
+		return errors.New("Password is not hashed"), ""
+	}
 	return nil, string(hash)
+}
