35160dfada1aef92c8389322823e4775f56115d8
Gusuv <papikanartur3@gmail.com>
2026-03-04 23:51:27 +0300

    feat: Init Repository and Service

--- internal/app/app.go
+++ internal/app/app.go
@@ -3,15 +3,22 @@ package app
 import (
 	"log/slog"
 	appgrpc "main/internal/app/grpc"
+	"main/internal/repository"
+	"main/internal/service"
 	"time"
+
+	"gorm.io/gorm"
 )
 
 type App struct {
 	GRPCServer *appgrpc.App
 }
 
-func New(log *slog.Logger, grpcPort int, storePath string, tokenTTL time.Duration) *App {
-	grpc := appgrpc.New(log, grpcPort)
+func New(log *slog.Logger, grpcPort int, tokenTTL time.Duration, db *gorm.DB) *App {
+
+	authRepo := repository.NewRepo(db)
+	authService := service.New(log, authRepo, tokenTTL)
+	grpc := appgrpc.New(log, grpcPort, authService)
 
 	return &App{
 		GRPCServer: grpc,
