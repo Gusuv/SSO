67d9eaead27db26ca165de2585919076c6d954d6
Gusuv <papikanartur3@gmail.com>
2026-03-05 23:57:01 +0300

    feat: Db connect updated

--- internal/database/dbConnect.go
+++ internal/database/dbConnect.go
@@ -1,19 +1,65 @@
 package db
 
 import (
+	"database/sql"
+	"fmt"
 	"log/slog"
-	"os"
+	"main/internal/config"
+	"time"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-func MustDbConnect(log *slog.Logger, dsn string) *gorm.DB {
-	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+func MustDbConnect(log *slog.Logger, cfg config.Config) *gorm.DB {
+	log.Info("Connecting to database")
+	db, err := gorm.Open(postgres.Open(cfg.DbDsn), &gorm.Config{})
 	if err != nil {
-		log.Error("Connection failed", slog.Any("error", err))
-		os.Exit(1)
+		log.Error("Connection failed", "error", err)
+		panic(fmt.Errorf("Database connect failed: %w", err))
 	}
+	dbSQL, err := db.DB()
+	if err != nil {
+		panic(fmt.Errorf("Can`t get SQL.db: %w", err))
+	}
+
+	checkConnection(dbSQL, log)
+
+	connectionPool(dbSQL, log, cfg.Env)
+	log.Info("Database connected")
 	return db
 
+}
+
+func checkConnection(db *sql.DB, log *slog.Logger) {
+	if err := db.Ping(); err != nil {
+		log.Error("Database ping failed", "error", err)
+		panic(fmt.Errorf("Database ping failed: %w", err))
+	}
+
+}
+
+func connectionPool(db *sql.DB, log *slog.Logger, env string) {
+
+	switch env {
+	case "local":
+		db.SetMaxIdleConns(5)
+		db.SetConnMaxLifetime(time.Hour)
+		db.SetMaxOpenConns(20)
+		db.SetConnMaxIdleTime(5 * time.Minute)
+
+	case "prod":
+		db.SetMaxIdleConns(25)
+		db.SetConnMaxLifetime(5 * time.Hour)
+		db.SetMaxOpenConns(100)
+		db.SetConnMaxIdleTime(30 * time.Minute)
+
+	default:
+		db.SetMaxIdleConns(5)
+		db.SetConnMaxLifetime(time.Hour)
+		db.SetMaxOpenConns(20)
+		db.SetConnMaxIdleTime(5 * time.Minute)
+		log.Warn("env is not defined", env)
+
+	}
 }
