f9e1d5af42fc664e052613d007b2f6c6976bb192
Gusuv <papikanartur3@gmail.com>
2026-03-10 23:56:07 +0300

    feat: Add mvp registration methods

--- internal/repository/authRepo.go
+++ internal/repository/authRepo.go
@@ -12,14 +12,28 @@ type AuthRep struct {
 }
 
 func NewRepo(db *gorm.DB) *AuthRep {
-
 	return &AuthRep{db: db}
 }
 
-func (a *AuthRep) CreateUser(ctx context.Context, username, email, passwordHash string) error {
-	return a.db.Create(&models.Users{
+func (a *AuthRep) CreateUser(ctx context.Context, username, email, passwordHash string) (error, int64) {
+	user := models.Users{
 		Username:     username,
 		Email:        email,
 		PasswordHash: passwordHash,
+	}
+
+	if err := a.db.WithContext(ctx).Create(&user).Error; err != nil {
+		return err, 0
+	}
+	return nil, user.Id
+
+}
+
+func (a *AuthRep) SetRole(ctx context.Context, id int64) error {
+	role := models.Roles{}
+	a.db.Where("role = ?", "User").First(&role)
+	return a.db.Create(&models.UsersRoles{
+		UserId: id,
+		RoleId: role.Id,
 	}).Error
 }
