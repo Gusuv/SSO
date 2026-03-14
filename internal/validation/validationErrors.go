bb681d55847d177205e5b69d3062a4b6f292f5ef
Gusuv <papikanartur3@gmail.com>
2026-03-14 21:04:37 +0300

    feat: Create validation errors

---
+++ internal/validation/validationErrors.go
@@ -0,0 +1,13 @@
+package validation
+
+import "errors"
+
+var (
+	ShortPassword    = errors.New("password is too short")
+	LongPassword     = errors.New("password is too long")
+	RequiredEmail    = errors.New("email is required")
+	RequiredPassword = errors.New("password is required")
+	RequiredUsername = errors.New("username is required")
+	RequiredAppId    = errors.New("app id is required")
+	InvalidEmail     = errors.New("email is invalid")
+)
