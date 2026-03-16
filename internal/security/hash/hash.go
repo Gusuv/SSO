32eac473360af2d0757db32dc124460b04312337
Gusuv <papikanartur3@gmail.com>
2026-03-16 23:57:58 +0300

    fix: Return internal bcrypt error

--- internal/security/hash/hash.go
+++ internal/security/hash/hash.go
@@ -1,8 +1,6 @@
 package hash
 
 import (
-	"errors"
-
 	"golang.org/x/crypto/bcrypt"
 )
 
@@ -10,7 +8,7 @@ func MakeHash(password string) (string, error) {
 	hash, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 
 	if err != nil {
-		return "", errors.New("Password is not hashed")
+		return "", err
 	}
 	return string(hash), nil
 }
