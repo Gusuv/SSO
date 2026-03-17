3d4ee918125edf30839548bd1b219ed2506f076c
Gusuv <papikanartur3@gmail.com>
2026-03-18 00:00:01 +0300

    feat: New errors

--- internal/service/authErrors.go
+++ internal/service/authErrors.go
@@ -6,4 +6,6 @@ var (
 	ErrUserAlreadyExist = errors.New("user already exist")
 	ErrUserCreating     = errors.New("error when creating a user")
 	ErrPasswordHashing  = errors.New("error when hashing password")
+	ErrUserNotFound     = errors.New("user not found")
+	ErrInvalidPassword  = errors.New("invalid password")
 )
