78939ab7ed7e843da5b7abf735ab5ad0d925d10d
Gusuv <papikanartur3@gmail.com>
2026-03-20 07:28:01 +0300

    chore: Users table migrations

---
+++ migrations/000001_create_users_table.up.sql
@@ -0,0 +1,9 @@
+-- up
+CREATE TABLE  users (
+    id SERIAL PRIMARY KEY,
+    username TEXT  NOT NULL UNIQUE,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT  NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+);
