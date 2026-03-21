a1ba2b8d47a73a8f04198e7b8a26b238364141b9
Gusuv <papikanartur3@gmail.com>
2026-03-21 23:43:58 +0300

    feat: Added migration runner

---
+++ cmd/migrator/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"main/internal/config"
+	"os"
+	"path/filepath"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func main() {
+
+	var cfgPath string
+	var migrationPath string
+	var step int
+
+	flag.StringVar(&cfgPath, "config", "", "path to config")
+	flag.StringVar(&migrationPath, "migration-path", "", "path to migration files")
+	flag.IntVar(&step, "step", 0, "step for migration")
+	flag.Parse()
+
+	if cfgPath == "" {
+		fmt.Println("config path is requitred")
+		os.Exit(1)
+	}
+	c := config.MustLoad(cfgPath)
+
+	dbUrl := c.DbUrl()
+
+	absPath, err := filepath.Abs(migrationPath)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	m, err := migrate.New("file://"+absPath, dbUrl)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	var migrateError error
+	if step != 0 {
+		migrateError = m.Steps(step)
+	} else {
+		migrateError = m.Up()
+	}
+
+	if migrateError != nil && !errors.Is(migrateError, migrate.ErrNoChange) {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("migrations applied successfully")
+}
