97563330bb3cd43a5f9de7bc68f15536b581f366
Gusuv <papikanartur3@gmail.com>
2026-03-29 22:47:33 +0300

    fix: Using user role int jwt

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -19,42 +19,41 @@ type AuthService struct {
 
 type AuthRepo interface {
 	TxCreateUser(ctx context.Context, username, email, passwordHash string) (int64, error)
-	GetUserByEmail(ctx context.Context, email string) (*models.Users, error)
+	GetUserByEmail(ctx context.Context, email string) (*models.Users, *models.Roles, error)
 }
-
 type AuthToken interface {
-	GenerateToken(userId, appID int64) (string, string, error)
+	GenerateToken(userId, appId int64, role string) (string, string, error)
 }
 
 func New(log *slog.Logger, rep AuthRepo, tokenTTL time.Duration, jwt AuthToken) *AuthService {
 	return &AuthService{log: log, rep: rep, tokenTTL: tokenTTL, jwt: jwt}
 }
 
-func (a *AuthService) UserLogin(ctx context.Context, email, password string, appID int64) (accessT string, refreshT string, userId int64, err error) {
-	user, err := a.rep.GetUserByEmail(ctx, email)
+func (a *AuthService) UserLogin(ctx context.Context, email, password string, appID int64) (accessT string, refreshT string, userId, expiresAt int64, err error) {
+	user, role, err := a.rep.GetUserByEmail(ctx, email)
 	if err != nil {
 		if errors.Is(err, repository.ErrUserNotFound) {
-			return "", "", 0, ErrUserNotFound
+			return "", "", 0, 0, ErrUserNotFound
 		}
 		a.log.Error("failed to get user", slog.Any("error", err))
-		return "", "", 0, err
+		return "", "", 0, 0, err
 	}
 
 	if !hash.HashCompare(user.PasswordHash, password) {
 		a.log.Warn("invalid password", slog.Int64("id", user.Id))
-		return "", "", 0, ErrInvalidPassword
+		return "", "", 0, 0, ErrInvalidPassword
 	}
 	a.log.Info("user successfully logged in",
 		slog.Int64("id", user.Id),
-		slog.String("Username", user.Username))
+		slog.String("username", user.Username))
 
-	accessToken, refreshToken, err := a.jwt.GenerateToken(user.Id, appID)
+	accessToken, refreshToken, err := a.jwt.GenerateToken(user.Id, appID, role.Role)
 	if err != nil {
-		a.log.Error("errror with jwt", slog.Any("err", err))
-		return "", "", 0, err
+		a.log.Error("error with jwt", slog.Any("err", err))
+		return "", "", 0, 0, err
 	}
 
-	return accessToken, refreshToken, user.Id, nil
+	return accessToken, refreshToken, user.Id, time.Now().Add(a.tokenTTL).Unix(), nil
 }
 
 func (a *AuthService) UserRegister(ctx context.Context, username, email, password string) (success bool, err error) {
