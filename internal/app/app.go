b29d227a027bc4831aeb348237516d456b9bddde
Gusuv <papikanartur3@gmail.com>
2026-03-31 23:46:05 +0300

    feat: Using new token function

--- internal/app/app.go
+++ internal/app/app.go
@@ -5,6 +5,7 @@ import (
 	appgrpc "main/internal/app/grpc"
 	"main/internal/config"
 	"main/internal/repository"
+	security "main/internal/security/jwt"
 	"main/internal/service"
 
 	"gorm.io/gorm"
@@ -16,8 +17,9 @@ type App struct {
 
 func New(log *slog.Logger, cfg *config.Config, db *gorm.DB) *App {
 
+	authJWT := security.NewToken(cfg.JWTSecret, cfg.TokenTTL)
 	authRepo := repository.NewRepo(db)
-	authService := service.New(log, authRepo, cfg.TokenTTL)
+	authService := service.New(log, authRepo, cfg.TokenTTL, authJWT)
 	grpcServer := appgrpc.New(log, cfg.Grpc.Port, authService)
 
 	return &App{
