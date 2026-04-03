07d8821052818af16c1380fe6d456f4b21694ea4
Gusuv <papikanartur3@gmail.com>
2026-04-03 14:10:48 +0300

    fix: Fixed struct

--- internal/models/refresh_tokens.go
+++ internal/models/refresh_tokens.go
@@ -5,7 +5,7 @@ import "time"
 type Refresh_tokens struct {
 	Id        int64 `gorm:"primary key"`
 	UserId    int64
-	tokenHash string
+	TokenHash string
 	CreatedAt *time.Time `gorm:"autoCreateTime"`
 	ExpiresAt *time.Time `gorm:"autoCreateTime"`
 }
