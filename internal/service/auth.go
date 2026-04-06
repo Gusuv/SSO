b9987d77e4d4aa464c6897fa39da89bb182cd22b
Gusuv <papikanartur3@gmail.com>
2026-04-06 23:57:39 +0300

    feat: Return JWT

--- internal/service/auth.go
+++ internal/service/auth.go
@@ -18,11 +18,20 @@ type AuthService struct {
 }
 
 type AuthRepo interface {
+	AddRefreshToken(ctx context.Context, jwt *models.JWT) error
+	GetUserWithRole(ctx context.Context, email string) (*models.Users, string, error)
 	TxCreateUser(ctx context.Context, username, email, passwordHash string) (int64, error)
-	GetUserByEmail(ctx context.Context, email string) (*models.Users, *models.Roles, error)
 }
+
+const (
+	loginOp    = "Auth.login"
+	registerOp = "Auth.register"
+	logoutOp   = "Auth.logout"
+	adminOp    = "Auth.admin"
+)
+
 type AuthToken interface {
-	GenerateToken(userId, appId int64, role string) (string, string, error)
+	GenerateToken(userId, appId int64, role string) (*models.JWT, error)
 }
 
 func New(log *slog.Logger, rep AuthRepo, tokenTTL time.Duration, jwt AuthToken) *AuthService {
@@ -30,37 +39,60 @@ func New(log *slog.Logger, rep AuthRepo, tokenTTL time.Duration, jwt AuthToken) 
 }
 
 func (a *AuthService) UserLogin(ctx context.Context, email, password string, appID int64) (accessT string, refreshT string, userId, expiresAt int64, err error) {
-	user, role, err := a.rep.GetUserByEmail(ctx, email)
+	loginLog := a.log.With("op", loginOp, "app_id", appID)
+	start := time.Now()
+
+	user, role, err := a.rep.GetUserWithRole(ctx, email)
 	if err != nil {
 		if errors.Is(err, repository.ErrUserNotFound) {
+			loginLog.Warn("user not found", slog.Any("err", err))
 			return "", "", 0, 0, ErrUserNotFound
 		}
-		a.log.Error("failed to get user", slog.Any("error", err))
+		loginLog.Error("failed to get user", slog.Any("err", err))
 		return "", "", 0, 0, err
 	}
 
 	if !hash.HashCompare(user.PasswordHash, password) {
-		a.log.Warn("invalid password", slog.Int64("id", user.Id))
+		loginLog.Warn("invalid password")
 		return "", "", 0, 0, ErrInvalidPassword
 	}
-	a.log.Info("user successfully logged in",
-		slog.Int64("id", user.Id),
-		slog.String("username", user.Username))
 
-	accessToken, refreshToken, err := a.jwt.GenerateToken(user.Id, appID, role.Role)
+	jwt, err := a.jwt.GenerateToken(user.Id, appID, role)
 	if err != nil {
-		a.log.Error("error with jwt", slog.Any("err", err))
+		loginLog.Error("error with jwt", slog.Any("err", err))
 		return "", "", 0, 0, err
 	}
+	refreshHash, err := hash.MakeHash(jwt.RefreshToken)
 
-	return accessToken, refreshToken, user.Id, time.Now().Add(a.tokenTTL).Unix(), nil
+	if err != nil {
+		loginLog.Error("refresh token hashing error", slog.Any("err", err))
+		return "", "", 0, 0, err
+	}
+	jwt.RefreshHash = refreshHash
+
+	if err := a.rep.AddRefreshToken(ctx, jwt); err != nil {
+
+		loginLog.Error("cant add refresh token to db", slog.Any("err", err))
+		return "", "", 0, 0, err
+
+	}
+
+	loginLog.Info("login success",
+		slog.Int64("user_id", user.Id),
+		slog.String("username", user.Username),
+		slog.Duration("latency", time.Since(start)),
+	)
+
+	return jwt.AccessToken, jwt.RefreshToken, user.Id, time.Now().Add(a.tokenTTL).Unix(), nil
 }
 
 func (a *AuthService) UserRegister(ctx context.Context, username, email, password string) (success bool, err error) {
+	regLog := a.log.With(slog.String("op", registerOp))
+	start := time.Now()
 
 	passwordHash, err := hash.MakeHash(password)
 	if err != nil {
-		a.log.Error("password hashing error", slog.Any("error", err))
+		regLog.Error("password hashing error", slog.Any("err", err))
 		return false, ErrPasswordHashing
 	}
 
@@ -71,21 +103,35 @@ func (a *AuthService) UserRegister(ctx context.Context, username, email, passwor
 		}
 
 		if errors.Is(err, repository.ErrSetRoleError) {
-			a.log.Error("register failed: set role error", slog.Any("error", err))
+			regLog.Error("register failed: set role error", slog.Any("err", err))
 			return false, ErrUserCreating
 		}
-		a.log.Error("register failed: something went wrong", slog.Any("error", err))
+		regLog.Error("register failed: something went wrong", slog.Any("err", err))
 		return false, err
 	}
 
-	a.log.Info("user successful registered",
-		slog.Int64("id", id),
-		slog.String("email", email))
-
+	regLog.Info("register success",
+		slog.Int64("user_id", id),
+		slog.String("email", email),
+		slog.Duration("latency", time.Since(start)),
+	)
 	return true, nil
 }
 
 func (a *AuthService) AdminCheck(ctx context.Context, accessToken string) (bool, error) {
-	// In development
+	/*  In development
+	admLog := a.log.With(slog.String("op", adminOp))
+	start := time.Now()
+	*/
+
 	return false, nil
+}
+
+func (a *AuthService) LogOut(ctx context.Context, refreshToken string) (string, error) {
+	/*   In development
+	 logOutLog := a.log.With(slog.String("op", logoutOp))
+	start := time.Now()
+	*/
+
+	return "", nil
 }
