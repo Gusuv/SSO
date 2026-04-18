575ffef4f714bd7cf260d3db96f2348801f16e40
Gusuv <papikanartur3@gmail.com>
2026-04-18 17:19:24 +0300

    feat: Add refresh and updated to fit a new proto

--- internal/grpc/auth/server.go
+++ internal/grpc/auth/server.go
@@ -3,6 +3,7 @@ package auth
 import (
 	"context"
 	"errors"
+	"main/internal/models"
 	"main/internal/service"
 	"main/internal/validation"
 	"strings"
@@ -11,21 +12,24 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type serverAPI struct {
 	sso1.UnimplementedAuthServiceServer
-	auth Auth
+	auth AuthService
 }
 
-type Auth interface {
-	UserLogin(ctx context.Context, email, password string, appID int64) (accessToken, refreshToken string, userId, expiresAt int64, err error)
-	UserRegister(ctx context.Context, username, email, password string) (success bool, err error)
-	AdminCheck(ctx context.Context, accessToken string) (bool, error)
-	LogOut(ctx context.Context, refreshToken string) (string, error)
+type AuthService interface {
+	UserLogin(ctx context.Context, email, password string) (loginRes service.LoginResult, err error)
+	UserRegister(ctx context.Context, username, email, password string) (userId int64, err error)
+	Logout(ctx context.Context, refreshToken string) (string, error)
+	RefreshToken(ctx context.Context, refreshToken string) (result *service.RefreshResult, err error)
+	GetMe(context.Context) (models.Users, string, error)
 }
 
-func Register(grpcServ *grpc.Server, auth Auth) {
+func Register(grpcServ *grpc.Server, auth AuthService) {
 	sso1.RegisterAuthServiceServer(grpcServ, &serverAPI{
 		auth: auth,
 	})
@@ -37,22 +41,29 @@ func (s *serverAPI) Login(ctx context.Context, req *sso1.LoginRequest) (*sso1.Lo
 	email := strings.TrimSpace(req.GetEmail())
 	password := req.GetPassword()
 
-	if err := validation.LoginValidation(email, password, req.GetAppId()); err != nil {
+	if err := validation.LoginValidation(email, password); err != nil {
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	accessToken, refreshToken, userId, expiresAt, err := s.auth.UserLogin(ctx, email, password, req.GetAppId())
+	res, err := s.auth.UserLogin(ctx, email, password)
 	if err != nil {
 		switch {
-		case errors.Is(err, service.ErrUserNotFound):
-			return nil, status.Error(codes.NotFound, "User not found")
-		case errors.Is(err, service.ErrInvalidPassword):
-			return nil, status.Error(codes.Unauthenticated, "Invalid password")
+		case errors.Is(err, service.ErrInvalidCredentials):
+			return nil, status.Error(codes.Unauthenticated, "Invalid credentials")
 		default:
 			return nil, status.Error(codes.Internal, "Something went wrong")
 		}
 	}
-	return &sso1.LoginResponse{AccessToken: accessToken, RefreshToken: refreshToken, UserId: userId, ExpiresAt: expiresAt}, nil
+	return &sso1.LoginResponse{
+		Tokens: &sso1.TokensPair{
+			AccessToken:  res.AccessToken,
+			RefreshToken: res.RefreshToken,
+			ExpiresAt: &timestamppb.Timestamp{
+				Seconds: res.ExpiresAt,
+			},
+		},
+		UserId: res.UserId,
+	}, nil
 }
 
 func (s *serverAPI) Register(ctx context.Context, req *sso1.RegisterRequest) (*sso1.RegisterResponse, error) {
@@ -64,7 +75,7 @@ func (s *serverAPI) Register(ctx context.Context, req *sso1.RegisterRequest) (*s
 		return nil, status.Error(codes.InvalidArgument, err.Error())
 	}
 
-	success, err := s.auth.UserRegister(ctx, username, email, password)
+	userId, err := s.auth.UserRegister(ctx, username, email, password)
 	if err != nil {
 		switch {
 		case errors.Is(err, service.ErrUserAlreadyExist):
@@ -83,13 +94,47 @@ func (s *serverAPI) Register(ctx context.Context, req *sso1.RegisterRequest) (*s
 
 	}
 
-	return &sso1.RegisterResponse{Success: success}, nil
+	return &sso1.RegisterResponse{UserId: userId}, nil
 }
 
 func (s *serverAPI) Logout(ctx context.Context, req *sso1.LogoutRequest) (*sso1.LogoutResponse, error) {
-	return &sso1.LogoutResponse{Message: "Successful user logout"}, nil
+	refreshToken := strings.TrimSpace(req.GetRefreshToken())
+	if refreshToken == "" {
+		return nil, status.Error(codes.InvalidArgument, "Refresh token is required")
+	}
+
+	return &sso1.LogoutResponse{}, nil
 }
 
-func (s *serverAPI) AdminCheck(ctx context.Context, req *sso1.AdminCheckRequest) (*sso1.AdminCheckResponse, error) {
-	return &sso1.AdminCheckResponse{IsAdmin: false}, nil
+func (s *serverAPI) Refresh(ctx context.Context, req *sso1.RefreshRequest) (*sso1.RefreshResponse, error) {
+	refreshToken := strings.TrimSpace(req.GetRefreshToken())
+	if refreshToken == "" {
+		return nil, status.Error(codes.InvalidArgument, "Refresh token is required")
+	}
+	tokens, err := s.auth.RefreshToken(ctx, refreshToken)
+	if err != nil {
+		switch {
+		case errors.Is(err, service.ErrInvalidRefreshToken):
+			return nil, status.Error(codes.InvalidArgument, "Invalid refresh token")
+		case errors.Is(err, service.ErrRefreshTokenExpired):
+			return nil, status.Error(codes.Unauthenticated, "Refresh token expired")
+		default:
+			return nil, status.Error(codes.Internal, err.Error())
+		}
+
+	}
+
+	return &sso1.RefreshResponse{
+		Tokens: &sso1.TokensPair{
+			AccessToken:  tokens.AccessToken,
+			RefreshToken: tokens.Refresh,
+			ExpiresAt: &timestamppb.Timestamp{
+				Seconds: tokens.ExpiresAt.Unix(),
+			},
+		}, UserId: tokens.UserId,
+	}, nil
+}
+
+func (s *serverAPI) GetMe(ctx context.Context, _ *emptypb.Empty) (*sso1.GetMeResponse, error) {
+	return &sso1.GetMeResponse{}, nil
 }
