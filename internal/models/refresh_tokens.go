557f437b40d6587adad07dfce215bec281fc3671
Gusuv <papikanartur3@gmail.com>
2026-04-18 17:22:10 +0300

    fix: Renamed struct and added revoked

--- internal/models/refresh_tokens.go
+++ internal/models/refresh_tokens.go
@@ -2,10 +2,11 @@ package models
 
 import "time"
 
-type Refresh_tokens struct {
+type RefreshTokens struct {
 	Id        int64 `gorm:"primary key"`
 	UserId    int64
 	TokenHash string
 	CreatedAt *time.Time `gorm:"autoCreateTime"`
 	ExpiresAt *time.Time `gorm:"autoCreateTime"`
+	Revoked   bool       `gorm:"default:false"`
 }
