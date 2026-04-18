a2f05faaeec79e2648a5c4c9a224f88326012a8d
Gusuv <papikanartur3@gmail.com>
2026-04-18 17:26:50 +0300

    feat: New errors

--- internal/repository/authRepoErrors.go
+++ internal/repository/authRepoErrors.go
@@ -3,7 +3,8 @@ package repository
 import "errors"
 
 var (
-	ErrUserExist    = errors.New("user already exist")
-	ErrSetRoleError = errors.New("can`t set user role")
-	ErrUserNotFound = errors.New("email not found")
+	ErrUserExist            = errors.New("user already exist")
+	ErrSetRoleError         = errors.New("can`t set user role")
+	ErrUserNotFound         = errors.New("email not found")
+	ErrRefreshTokenNotFound = errors.New("refresh token not found")
 )
