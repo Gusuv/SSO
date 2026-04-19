3d2f6575d896a555519a9087fa8f0cf3daeacb79
Gusuv <papikanartur3@gmail.com>
2026-04-19 19:15:21 +0300

    feat: Add separate access/refresh token generation

--- internal/security/jwt/jwt.go
+++ internal/security/jwt/jwt.go
@@ -1,7 +1,7 @@
 package security
 
 import (
-	"main/internal/models"
+	"main/internal/config"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -10,47 +10,73 @@ import (
 
 type accessClaims struct {
 	UserId int64
-	Role   string
-	AppId  int64
+	Role   []string
 	jwt.RegisteredClaims
 }
 
 type JWTService struct {
 	Secret   []byte
-	TokenTTL time.Duration
+	TokenTTL config.TokensTTL
 }
 
-func NewToken(secret string, tokenTTL time.Duration) *JWTService {
+func NewToken(secret string, tokenTTL config.TokensTTL) *JWTService {
 	return &JWTService{
 		Secret:   []byte(secret),
 		TokenTTL: tokenTTL,
 	}
 }
 
-func (j *JWTService) GenerateToken(userId, appId int64, role string) (*models.JWT, error) {
+func (j *JWTService) GenerateTokens(userId int64, role []string) (*Tokens, error) {
+	now := time.Now()
 
-	claims := accessClaims{
-		UserId: userId,
-		Role:   role,
-		AppId:  appId,
-		RegisteredClaims: jwt.RegisteredClaims{
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(j.TokenTTL)),
-			IssuedAt:  jwt.NewNumericDate(time.Now()),
-		}}
-
-	accesToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString(j.Secret)
+	accessToken, err := j.GenerateAccessToken(userId, role)
 	if err != nil {
 		return nil, err
 	}
 
 	refreshToken := uuid.NewString()
 
-	jwt := models.JWT{
-		AccessToken:  accesToken,
-		RefreshToken: refreshToken,
-		UserId:       userId,
-		ExpiresAt:    time.Now().Add(j.TokenTTL),
+	jwt := Tokens{
+		AccessToken:      accessToken.AccessToken,
+		AccessExpiresAt:  accessToken.AccessExpiresAt,
+		RefreshExpiresAt: now.Add(j.TokenTTL.Refresh),
+		UserId:           userId,
+		RefreshToken:     refreshToken,
+	}
+	return &jwt, nil
+}
+
+func (j *JWTService) GenerateAccessToken(userID int64, role []string) (*AccessToken, error) {
+	now := time.Now()
+	claims := accessClaims{
+		UserId: userID,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(now.Add(j.TokenTTL.Access)),
+			IssuedAt:  jwt.NewNumericDate(now),
+		},
 	}
 
-	return &jwt, nil
+	accessToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString(j.Secret)
+	if err != nil {
+		return nil, err
+	}
+
+	return &AccessToken{
+		AccessToken:     accessToken,
+		AccessExpiresAt: now.Add(j.TokenTTL.Access),
+	}, nil
+}
+
+type AccessToken struct {
+	AccessToken     string
+	AccessExpiresAt time.Time
+}
+
+type Tokens struct {
+	AccessToken      string
+	AccessExpiresAt  time.Time
+	RefreshExpiresAt time.Time
+	UserId           int64
+	RefreshToken     string
 }
