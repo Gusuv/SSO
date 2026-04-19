01449e41412f137783eba768583b1e6c232431c7
Gusuv <papikanartur3@gmail.com>
2026-04-19 18:49:15 +0300

    feat: Add new errors

--- internal/service/authErrors.go
+++ internal/service/authErrors.go
@@ -3,9 +3,10 @@ package service
 import "errors"
 
 var (
-	ErrUserAlreadyExist = errors.New("user already exist")
-	ErrUserCreating     = errors.New("error when creating a user")
-	ErrPasswordHashing  = errors.New("error when hashing password")
-	ErrUserNotFound     = errors.New("user not found")
-	ErrInvalidPassword  = errors.New("invalid password")
+	ErrUserAlreadyExist    = errors.New("user already exist")
+	ErrUserCreating        = errors.New("error when creating a user")
+	ErrPasswordHashing     = errors.New("error when hashing password")
+	ErrInvalidCredentials  = errors.New("invalid credentials")
+	ErrInvalidRefreshToken = errors.New("invalid refresh token")
+	ErrRefreshTokenExpired = errors.New("refresh token expired")
 )
